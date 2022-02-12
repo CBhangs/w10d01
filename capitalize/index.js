750d22c6146dc705dff1de1dc6889ba7a2927f38
CBhangs <94818557+CBhangs@users.noreply.github.com>
2022-02-12 14:36:38 -0500

    Update index.js

--- capitalize/index.js
+++ capitalize/index.js
@@ -1,12 +1,12 @@
 const capitalize =(str)=>{
-    let arrString = str.split(' ‘);
+    let arrString = str.split(" ");
    //The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
     const final = arrString.map(element => {
         let firstChar = element.charAt(0).toUpperCase();
         let otherChar = element.slice(1);
         return firstChar + otherChar
     });
-    return final.join(’ ’);
+    return final.join(" ");
 }
 
 module.exports ={
