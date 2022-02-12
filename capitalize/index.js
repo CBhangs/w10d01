676208ffbf8f0906a058e0521096aad052e5dd1d
CBhangs <94818557+CBhangs@users.noreply.github.com>
2022-02-12 09:53:03 -0500

    Update index.js

--- capitalize/index.js
+++ capitalize/index.js
@@ -1,17 +1,14 @@
-function capitalize(str) {
-    const capitalizes = str => {
-        let arrString = str.split(" ");
-        const returnString = [];
-        
-        arrString.forEach (item => {
-            let firstChar = item[0].toUpperCase();
-            let otherLetters = item.slice(1);
-            returnString.push(firstChar.concat(otherLetters));
-        });
-
-        return returnString.join(" ");
-        }
+const capitalize =(str)=>{
+    let arrString = str.split(' ‘);
+   //The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
+    const final = arrString.map(element => {
+        let firstChar = element.charAt(0).toUpperCase();
+        let otherChar = element.slice(1);
+        return firstChar + otherChar
+    });
+    return final.join(’ ’);
 }
+
 module.exports ={
     capitalize
 }
